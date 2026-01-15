c0b0162780d451e8cec89719dd35cc89995506b3
TECHNETWORKINGSOLUTIONS <82411304+TECHNETWORKINGSOLUTIONS@users.noreply.github.com>
2026-01-15 10:38:10 -0500

    Delete tns-mail-portal-pro-premium directory

--- tns-mail-portal-pro-premium/worker/src/index.js
+++
@@ -1,8 +0,0 @@
-
-import { Queue, Worker, QueueScheduler } from 'bullmq';
-import IORedis from 'ioredis';
-
-const connection = new IORedis({ host: process.env.REDIS_HOST || 'redis', port: +(process.env.REDIS_PORT||6379) });
-new QueueScheduler('dns', { connection });
-new Worker('dns', async () => true, { connection });
-console.log('[worker] started');
